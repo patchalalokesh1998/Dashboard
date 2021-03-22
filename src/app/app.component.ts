29740b83858b100509ecdeb4c92800655ef2e3f8
P Lokesh <lokesh.patchala@absyz.com>
2021-03-22 12:31:59 +0530

    'headerchanges'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dashboard';
+  mobileQuery: MediaQueryList;
+
   showFiller = false;
+  private _mobileQueryListener: () => void;
+
+  constructor(private media: MediaMatcher,changeDetectorRef: ChangeDetectorRef)
+  {
+   this.mobileQuery=media.matchMedia('(max-width: 600px)');
+  //  this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+  //  this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 }
